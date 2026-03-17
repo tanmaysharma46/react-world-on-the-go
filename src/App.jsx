35c345afd1d542e9172a60840978f2c654299ff3
Tanmay Sharma <tanmay@kali.tanmay>
2026-03-18 00:41:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Countries from '../components/Countries'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>React World on the go ....</h1>
+   
+
+
+<Countries></Countries>
+
+    </>
+  )
+}
+
+export default App
